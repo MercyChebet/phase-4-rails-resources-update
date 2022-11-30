6b9c7b9c3e60c365296227e72e03f13628c4b131
MercyChebet <chebetm96@gmail.com>
2022-11-30 10:06:40 +0300

    Completed Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -27,5 +27,20 @@ class BirdsController < ApplicationController
   def bird_params
     params.permit(:name, :species)
   end
+   # rest of actions here...
+
+  # PATCH /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+  def bird_params
+    params.permit(:name, :species, :likes)
+  end
 
 end
